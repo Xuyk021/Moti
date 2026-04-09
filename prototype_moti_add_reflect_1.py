4b90d9c65ec02864a8ece3591445c7e661607651
Xuyk021 <1455178537@qq.com>
2026-04-09 14:13:15 -0400

    update the condition

--- prototype_moti_add_reflect_1.py
+++ prototype_moti_add_reflect_1.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import re
 
 import experiment_config_1 as CFG
-import moti_setting_config_meta as MSC
+import moti_setting_config_3 as MSC
 
 answer = [
     "There are no psychoactive effects or adverse events associated with CBD administration. CBD is found to be free from the risk of unintentional poisoning and the threat of interaction with other medicines. Moreover, CBD is better than cholesterol-reducing medicine in that, it reduces cholesterol much faster than cholesterol-reducing medicine. There are so many products on the market to try. You may come out feeling less anxious, and more relaxed."
